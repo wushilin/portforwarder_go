b0fa536701d9c388317ff81bb1f748f71ef93fc8
Wu Shilin <wushilin.sg@gmail.com>
2023-05-22 09:41:19 +0800

    Initial

---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	. "github.com/wushilin/portforwarder_go/logging"
+	"github.com/wushilin/portforwarder_go/worker"
+)
+
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return strings.Join([]string(*i), ",")
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+var binding arrayFlags
+var logLevel int
+var Usage = func() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+
+	flag.PrintDefaults()
+}
+
+func main() {
+	flag.Var(&binding, "b", "(multiple) Binding in [bind_address]:port::target_address:port format (e.g. :22::remote.host.com:22)")
+	flag.IntVar(&logLevel, "loglevel", 0, "Log level (0 for debug, higher is less)")
+	flag.Parse()
+	INFO().Msgf("Setting logging level to %d", logLevel)
+	if len(binding) == 0 {
+		Usage()
+		os.Exit(1)
+	}
+
+	wg := new(sync.WaitGroup)
+	workers := make([]*worker.WorkerConfig, 0)
+	for _, spec := range binding {
+		config, err := parse(spec)
+		if err != nil {
+			FATAL().Msgf("Error: %s", err)
+			os.Exit(1)
+		}
+		wg.Add(1)
+		workers = append(workers, config)
+		go config.Start(wg)
+	}
+	go report(workers)
+
+	wg.Wait()
+}
+
+func parse(input string) (config *worker.WorkerConfig, err error) {
+	indexCC := strings.Index(input, "::")
+	defaultError := errors.New(fmt.Sprintf("Invalid spec: %s", input))
+	if indexCC == -1 {
+		return nil, defaultError
+	}
+
+	listenSpec := input[:indexCC]
+	targetSpec := input[indexCC+2:]
+
+	listenIndexC := strings.Index(listenSpec, ":")
+	if listenIndexC == -1 {
+		return nil, defaultError
+	}
+	listen := listenSpec[:listenIndexC]
+	listenPortStr := listenSpec[listenIndexC+1:]
+	listenPort, err := strconv.Atoi(listenPortStr)
+	if err != nil {
+		return nil, defaultError
+	}
+
+	targetIndexC := strings.Index(targetSpec, ":")
+	if targetIndexC == -1 {
+		return nil, defaultError
+	}
+	target := targetSpec[:targetIndexC]
+	targetPortStr := targetSpec[targetIndexC+1:]
+	targetPort, err := strconv.Atoi(targetPortStr)
+	if err != nil {
+		return nil, defaultError
+	}
+	return &worker.WorkerConfig{
+		BindAddress: listen,
+		BindPort:    listenPort,
+		TargetHost:  target,
+		TargetPort:  targetPort,
+	}, nil
+}
+
+func report(ws []*worker.WorkerConfig) {
+	for {
+		for _, next := range ws {
+			INFO().Msgf(" *** STATUS for %s:%d->%s:%d uploaded %d bytes; downloaded %d bytes; active requests %d; total requests %d",
+				next.BindAddress, next.BindPort, next.TargetHost, next.TargetPort,
+				next.Uploaded, next.Downloaded, next.Active, next.TotalHandled)
+		}
+		time.Sleep(30 * time.Second)
+	}
+}
---
+++ worker/worker.go
@@ -0,0 +1,92 @@
+package worker
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	. "github.com/wushilin/portforwarder_go/logging"
+)
+
+type WorkerConfig struct {
+	BindAddress  string
+	BindPort     int
+	TargetHost   string
+	TargetPort   int
+	Uploaded     uint64
+	Downloaded   uint64
+	TotalHandled int64
+	Active       int64
+}
+
+var ID_GEN uint64 = 0
+
+func (v *WorkerConfig) Start(wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+	}()
+	listener := fmt.Sprintf("%s:%d", v.BindAddress, v.BindPort)
+	server, err := net.Listen("tcp4", listener)
+	if err != nil {
+		ERROR().Msgf("Failed to LISTEN to %s:%s", listener, err)
+		os.Exit(1)
+	}
+	defer server.Close()
+	INFO().Msgf("Listening %s:%d -> %s:%d", v.BindAddress, v.BindPort, v.TargetHost, v.TargetPort)
+	for {
+		connection, err := server.Accept()
+		if err != nil {
+			ERROR().Msgf("Error accepting connection: %s ", err.Error())
+			os.Exit(1)
+		}
+		var connection_id = atomic.AddUint64(&ID_GEN, 1)
+		INFO().Msgf("%d Accepted from %v to %v", connection_id, connection.RemoteAddr(), connection.LocalAddr())
+		go v.processClient(connection, connection_id)
+	}
+}
+
+func (v *WorkerConfig) processClient(connection net.Conn, conn_id uint64) {
+	atomic.AddInt64(&v.Active, 1)
+	start := time.Now()
+	var uploaded uint64 = 0
+	var downloaded uint64 = 0
+	defer func() {
+		connection.Close()
+		INFO().Msgf("%d Done. Uptime: %v Uploaded: %d bytes Downloaded: %d bytes", conn_id, time.Since(start), uploaded, downloaded)
+		atomic.AddUint64(&v.Uploaded, uploaded)
+		atomic.AddUint64(&v.Downloaded, downloaded)
+		atomic.AddInt64(&v.Active, -1)
+		atomic.AddInt64(&v.TotalHandled, 1)
+	}()
+
+	// client hello must be read in 30 seconds
+	dest, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", v.TargetHost, v.TargetPort), 10*time.Second)
+	if err != nil {
+		WARN().Msgf("%d Client %v can't connect to host %s: %s", conn_id, connection.RemoteAddr(), v.TargetHost, err)
+		return
+	}
+	INFO().Msgf("%d Client %v connected to host %s via %s", conn_id, connection.RemoteAddr(), v.TargetHost, dest.LocalAddr())
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go pipe(conn_id, &uploaded, &downloaded, connection, dest, wg)
+	wg.Wait()
+}
+
+func pipe(conn_id uint64, uploaded *uint64, downloaded *uint64, src net.Conn, dest net.Conn, wg *sync.WaitGroup) {
+	go func() {
+		defer wg.Done()
+		written, _ := io.Copy(src, dest)
+		atomic.AddUint64(downloaded, uint64(written))
+		src.Close()
+	}()
+	go func() {
+		defer wg.Done()
+		written, _ := io.Copy(dest, src)
+		atomic.AddUint64(uploaded, uint64(written))
+		dest.Close()
+	}()
+}
